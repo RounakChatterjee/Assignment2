931a1a69be7436ad9fdec3d3c4b05343fc48e648
RounakChatterjee <56823119+RounakChatterjee@users.noreply.github.com>
2019-12-03 01:04:47 +0530

    Update array6.c

--- array6.c
+++ array6.c
@@ -1,3 +1,5 @@
+/*This program depicts the way to write data to a binary file and then extract data from bunary file to be used for calculations
+The path depicted is my computer local address and must be changed to the save address of the computer in use to get desired results*/
 #include<stdio.h>
 #include<stdlib.h>
 float* stats(float arr[],int len)// a functin that takes array of arbitary
@@ -25,15 +27,13 @@ int main(void)
 	int i = 0;
 	for(i = 0;i<100;i++)
 		{p[i] = (i+1)*(i+1);}
-	/*printf("The Mean of the numbers = %f\n",stats(p,100)[0]);
-	printf("The variance of the numbers = %f\n",stats(p,100)[1]);*/
 	FILE *op;
 	op = fopen( "file3.bin" , "wb" );
 	 float* str = stats(p,100);
     fwrite(str , 4 , sizeof(float) , op );
    fclose(op);
 	FILE *fp;
-	 fp = fopen( "C:/Users/ROUNAK/Desktop/c programs/file3.bin" , "rb" );
+	 fp = fopen( "C:/Users/ROUNAK/Desktop/c programs/file3.bin" , "rb" );// path must be changed to local location for working of the program
 	 float* out;
     fread(out , 4 , sizeof(float) , fp );
    fclose(fp);
