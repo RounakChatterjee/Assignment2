5b22972a868ec9fe6d6125488b80eb6e0a454047
RounakChatterjee <56823119+RounakChatterjee@users.noreply.github.com>
2019-12-03 03:16:49 +0530

    Update pyfact.py

--- pyfact.py
+++ pyfact.py
@@ -4,9 +4,9 @@ This program calculates factorial using recusrion  technique
 import timeit as t
 def fact(n):
 	if(n == 1):
-		return 1;
+		return 1
 	else:
 		return n*fact(n-1)
 n = int(input("Enter a number = "))
 print("The factorial of the number is = ",fact(n))
-print("Time taken = ",t.timeit());
+print("Time taken = ",t.timeit())
