a2230f052903c02cd03a0e60257998e7b0898bc4
RounakChatterjee <56823119+RounakChatterjee@users.noreply.github.com>
2019-12-03 02:57:57 +0530

    Update pycfact.py

--- pycfact.py
+++ pycfact.py
@@ -5,7 +5,9 @@ the CMD command for creating dll file is:
 gcc -shared -o libcalci.dll -fPIC factorial.c
 
 This factorial.c is the actual factorial.c file that we created in C to compute the factorial
+
+for sake of convenience I have also uploaded the dll file
 '''
 from ctypes import *
-fact = CDLL("C:/Users/ROUNAK/Desktop/c programs/libcalci.dll")
+fact = CDLL("C:/Users/ROUNAK/Desktop/c programs/libcalci.dll") #the path name must be changed to local to execute it properly
 fact.main()
